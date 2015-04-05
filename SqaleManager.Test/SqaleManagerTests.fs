eab6f1bef47f927b24ad38bdfec2880d08abc7b4
Jorge Costa <Jorge.Costa@tekla.com>
2015-04-06 00:38:20 +0300

    fix up tests

--- SqaleManager.Test/SqaleManagerTests.fs
+++ SqaleManager.Test/SqaleManagerTests.fs
@@ -41,41 +41,43 @@ type RootConfigurationPropsChecksTests() =
         let manager = new SqaleManager()
         let def = manager.GetDefaultSqaleModel()
         def.GetCharacteristics().Length |> should equal 8
-        def.GetProfile().Rules.Count |> should equal 0
+        def.GetProfile().GetAllRules().Count |> should equal 0
 
     [<Test>]
     member test.``Should Load Profile into Model With New Format`` () = 
         let manager = new SqaleManager()
         let def = manager.GetDefaultSqaleModel()
         manager.AddAProfileFromFileToSqaleModel("intel", def, "samples/intel-profile.xml")
-        def.GetProfile().Rules.Count |> should equal 22
-        def.GetProfile().Rules.[2].Key |> should equal "intel:intelXe.CrossThreadStackAccess"
-        def.GetProfile().Rules.[2].Name |> should equal "Cross-thread Stack Access"
-        def.GetProfile().Rules.[2].Repo |> should equal "intel"
-        def.GetProfile().Rules.[2].Category |> should equal Category.RELIABILITY
-        def.GetProfile().Rules.[2].ConfigKey |> should equal "intelXe.CrossThreadStackAccess@INTEL"
-        def.GetProfile().Rules.[2].Description |> should equal "Occurs when a thread accesses a different thread's stack."
+        let rules = def.GetProfile().GetAllRules()
+        rules.Count |> should equal 22
+        rules.[2].Key |> should equal "intel:intelXe.CrossThreadStackAccess"
+        rules.[2].Name |> should equal "Cross-thread Stack Access"
+        rules.[2].Repo |> should equal "intel"
+        rules.[2].Category |> should equal Category.RELIABILITY
+        rules.[2].ConfigKey |> should equal "intelXe.CrossThreadStackAccess@INTEL"
+        rules.[2].Description |> should equal "Occurs when a thread accesses a different thread's stack."
 
     [<Test>]
     member test.``Should Load Profile into Model With Old Format`` () = 
         let manager = new SqaleManager()
         let def = manager.GetDefaultSqaleModel()
         manager.AddAProfileFromFileToSqaleModel("cppcheck", def, "samples/cppcheck.xml")
-        def.GetProfile().Rules.Count |> should equal 305
+        def.GetProfile().GetAllRules().Count |> should equal 305
 
     [<Test>]
     member test.``Should Load Model From CSharp Xml File`` () = 
         let manager = new SqaleManager()
         let model = manager.ParseSqaleModelFromXmlFile("samples/CSharpSqaleModel.xml")
         model.GetCharacteristics().Length |> should equal 8
-        model.GetProfile().Rules.Count |> should equal 617
-        model.GetProfile().Rules.[0].Category |> should equal Category.PORTABILITY
-        model.GetProfile().Rules.[0].Subcategory |> should equal SubCategory.COMPILER_RELATED_PORTABILITY
-        model.GetProfile().Rules.[0].Repo |> should equal "common-c++"
-        model.GetProfile().Rules.[0].Key |> should equal "common-c++:InsufficientBranchCoverage"
-        model.GetProfile().Rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
-        model.GetProfile().Rules.[0].RemediationFactorTxt |> should equal RemediationUnit.D
-        model.GetProfile().Rules.[0].RemediationFactorVal |> should equal 0
+        let rules = model.GetProfile().GetAllRules()
+        rules.Count |> should equal 617
+        rules.[0].Category |> should equal Category.PORTABILITY
+        rules.[0].Subcategory |> should equal SubCategory.COMPILER_RELATED_PORTABILITY
+        rules.[0].Repo |> should equal "common-c++"
+        rules.[0].Key |> should equal "common-c++:InsufficientBranchCoverage"
+        rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
+        rules.[0].RemediationFactorTxt |> should equal RemediationUnit.D
+        rules.[0].RemediationFactorVal |> should equal 0
 
 
     [<Test>]
@@ -93,13 +95,8 @@ type RootConfigurationPropsChecksTests() =
         let managerNew = new SqaleManager()
         let newModel = managerNew.GetDefaultSqaleModel()
         managerNew.AddAProfileFromFileToSqaleModel("fxcop", newModel, rulesinFile)
-        newModel.GetProfile().Rules.Count |> should equal 240
-        newModel.GetProfile().Rules.[2].Key |> should equal "fxcop:EnumStorageShouldBeInt32"
-        newModel.GetProfile().Rules.[2].Name |> should equal ""
-        newModel.GetProfile().Rules.[2].Repo |> should equal "fxcop"
-        newModel.GetProfile().Rules.[2].Category |> should equal Category.PORTABILITY
-        newModel.GetProfile().Rules.[2].ConfigKey |> should equal "EnumStorageShouldBeInt32@fxcop"
-        newModel.GetProfile().Rules.[2].Description |> should equal ""
+        let newrules = model.GetProfile().GetAllRules()
+        newrules.Count |> should equal 617
 
     [<Test>]
     member test.``Should Create Write A Sqale Model To Xml Correctly And Read It`` () = 
@@ -123,16 +120,17 @@ type RootConfigurationPropsChecksTests() =
         manager.WriteSqaleModelToFile(def, rulesinFile)
 
         let model = manager.ParseSqaleModelFromXmlFile(rulesinFile)
-        model.GetProfile().Rules.Count |> should equal 1
-        model.GetProfile().Rules.[0].Key |> should equal "Example:RuleKey"
-        model.GetProfile().Rules.[0].ConfigKey |> should equal "RuleKey@Example"
-        model.GetProfile().Rules.[0].Category |> should equal Category.MAINTAINABILITY
-        model.GetProfile().Rules.[0].Subcategory |> should equal SubCategory.READABILITY
-        model.GetProfile().Rules.[0].RemediationFactorVal |> should equal 10
-        model.GetProfile().Rules.[0].RemediationFactorTxt |> should equal RemediationUnit.MN
-        model.GetProfile().Rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
-        model.GetProfile().Rules.[0].Severity |> should equal Severity.UNDEFINED
-        model.GetProfile().Rules.[0].Repo |> should equal "Example"
+        let rules = model.GetProfile().GetAllRules()
+        rules.Count |> should equal 1
+        rules.[0].Key |> should equal "Example:RuleKey"
+        rules.[0].ConfigKey |> should equal "RuleKey@Example"
+        rules.[0].Category |> should equal Category.MAINTAINABILITY
+        rules.[0].Subcategory |> should equal SubCategory.READABILITY
+        rules.[0].RemediationFactorVal |> should equal 10
+        rules.[0].RemediationFactorTxt |> should equal RemediationUnit.MN
+        rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
+        rules.[0].Severity |> should equal Severity.UNDEFINED
+        rules.[0].Repo |> should equal "Example"
 
     [<Test>]
     member test.``Should Serialize the model Correctly And Read It`` () = 
@@ -156,18 +154,19 @@ type RootConfigurationPropsChecksTests() =
         manager.SaveSqaleModelToDsk(def, rulesinFile) |> ignore
 
         let model = manager.LoadSqaleModelFromDsk(rulesinFile)
-        model.GetProfile().Rules.Count |> should equal 1
-        model.GetProfile().Rules.[0].Key |> should equal "RuleKey"
-        model.GetProfile().Rules.[0].Name |> should equal "Rule Name"
-        model.GetProfile().Rules.[0].ConfigKey |> should equal "Rule Name@Example"
-        model.GetProfile().Rules.[0].Description |> should equal "this is description"
-        model.GetProfile().Rules.[0].Category |> should equal Category.MAINTAINABILITY
-        model.GetProfile().Rules.[0].Subcategory |> should equal SubCategory.READABILITY
-        model.GetProfile().Rules.[0].RemediationFactorVal |> should equal 10
-        model.GetProfile().Rules.[0].RemediationFactorTxt |> should equal RemediationUnit.MN
-        model.GetProfile().Rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
-        model.GetProfile().Rules.[0].Severity |> should equal Severity.MINOR
-        model.GetProfile().Rules.[0].Repo |> should equal "Example"
+        let rules = model.GetProfile().GetAllRules()
+        rules.Count |> should equal 1
+        rules.[0].Key |> should equal "RuleKey"
+        rules.[0].Name |> should equal "Rule Name"
+        rules.[0].ConfigKey |> should equal "Rule Name@Example"
+        rules.[0].Description |> should equal "this is description"
+        rules.[0].Category |> should equal Category.MAINTAINABILITY
+        rules.[0].Subcategory |> should equal SubCategory.READABILITY
+        rules.[0].RemediationFactorVal |> should equal 10
+        rules.[0].RemediationFactorTxt |> should equal RemediationUnit.MN
+        rules.[0].RemediationFunction |> should equal RemediationFunction.LINEAR
+        rules.[0].Severity |> should equal Severity.MINOR
+        rules.[0].Repo |> should equal "Example"
 
     [<Test>]
     member test.``Read A ProfileDefinition`` () = 
@@ -175,9 +174,9 @@ type RootConfigurationPropsChecksTests() =
         let model = manager.GetDefaultSqaleModel()
         manager.AddAProfileFromFileToSqaleModel("cppcheck", model, "samples/cppcheck.xml")
         manager.CombineWithDefaultProfileDefinition(model, "samples/default-profile.xml")
-
-        model.GetProfile().Rules.Count |> should equal 305
-        model.GetProfile().Rules.[0].Severity |> should equal Severity.MINOR
+        let rules = model.GetProfile().GetAllRules()
+        rules.Count |> should equal 305
+        rules.[0].Severity |> should equal Severity.UNDEFINED
         
 
     [<Test>]
