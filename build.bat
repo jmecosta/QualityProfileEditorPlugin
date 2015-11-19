f7e993536d19261db283dac50928c94edcc07927
Jorge Costa <Jorge.Costa@tekla.com>
2015-11-19 12:08:54 +0200

    fix compilation files

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-msbuild BuildPlugin.msbuild /p:VisualStudioVersion=12.0 /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No /p:EndVSQFile=QualityEditorPlugin.VSQ > buildlog2013.txt
+msbuild BuildPlugin.msbuild /p:VisualStudioVersion=12.0 /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No /p:EndVSQFile=QualityEditorPlugin2013.VSQ > buildlog2013.txt
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-msbuild BuildPlugin.msbuild /p:VisualStudioVersion=14.0 /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No /p:EndVSQFile=QualityEditorPlugin.VSQ > buildlog2015.txt
+msbuild BuildPlugin.msbuild /p:VisualStudioVersion=14.0 /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No /p:EndVSQFile=QualityEditorPlugin2015.VSQ > buildlog2015.txt
